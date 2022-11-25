fd42b2241d6ab3d686ddb2535db427c728b3097f
Ilya Snitavets <ilya.snitavets@opsguru.io>
2022-11-25 12:48:51 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,29 +23,29 @@ provider "google" {
   region = var.google_region
 }
 
-resource "google_storage_bucket" "bucket" {
-  name                        = "${var.google_project_id}-gcf-source" # Every bucket name must be globally unique
+resource "google_storage_bucket" "gcf-source-bucket" {
+  name                        = "${var.google_project_id}-gcf-source"
   location                    = "US"
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_object" "object" {
-  name   = "sourcecode.${timestamp()}.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "function-source.zip" # Add path to the zipped function source code
+resource "google_storage_bucket_object" "profile-service-source" {
+  name   = "profile-service-source.${timestamp()}.zip"
+  bucket = google_storage_bucket.gcf-source-bucket.name
+  source = "profile-service-source.zip"
 }
 
-resource "google_cloudfunctions_function" "function" {
-  name                  = "function-v2"
-  description           = "a new function"
+resource "google_cloudfunctions_function" "profile-service" {
+  name                  = "profile-service"
+  description           = "Function to hande profile service"
   runtime               = "nodejs16"
   available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.bucket.name
-  source_archive_object = google_storage_bucket_object.object.name
+  source_archive_bucket = google_storage_bucket.gcf-source-bucket.name
+  source_archive_object = google_storage_bucket_object.profile-service-source.name
   trigger_http          = true
   entry_point           = "profile"
 }
 
 output "function_uri" {
-  value = google_cloudfunctions_function.function.https_trigger_url
+  value = google_cloudfunctions_function.profile-service.https_trigger_url
 }
